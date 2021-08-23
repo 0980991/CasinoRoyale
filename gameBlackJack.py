ace8bfb33bd0dc2c194bd63f23519c6ae7b0aa16
MauriceM <0980991@hr.nl>
2021-08-23 10:40:32 +0200

    Blackjack __init__

--- gameBlackJack.py
+++ gameBlackJack.py
@@ -1,37 +1,45 @@
 import InputFunctions as inf
 
 class BlackJack:
-    def start(self, deck):
-        roundcount = 1
-        gamenotover = True
-        playerholds = False
+    def __init(self)
+        self.deck = None
+        self.gamenotover = True
+        self.roundcount = 1
+        self.playerholds = False
 
-        playerhand = deck.pull2RandomCards()
-        dealerhand = [deck.pullRandomCard()] # The dealer only shows 1 card at first
+        self.playerhand = None
+        self.dealerhand = None
+
+    def start(self, deck):
+        self.deck = deck
+
+
+        self.playerhand = self.deck.pull2RandomCards()
+        self.dealerhand = [self.deck.pullRandomCard()] # The dealer only shows 1 card at first
 
         while gamenotover:
 
             #  Print player hand
             inf.prettyPrint('Your hand is:')
-            inf.printHand(playerhand)
+            inf.printHand(self.playerhand)
 
             #  Has the player busted?
-            if deck.sumCards(playerhand) > 21:
+            if self.deck.sumCards(self.playerhand) > 21:
                 inf.prettyPrint('YOU\'VE BUSTED')
                 return [False, inf.playAgain()]
 
             #  Does the player have 21?
-            elif deck.sumCards(playerhand) == 21:
+            elif self.deck.sumCards(self.playerhand) == 21:
                 inf.prettyPrint('CONGRATULATIONS YOU\'VE GOT 21')
 
                 return [True, inf.playAgain()]
 
             #  Print dealer hand
             inf.prettyPrint(f'The dealer\'s hand is:')
-            inf.printHand(dealerhand)
+            inf.printHand(self.dealerhand)
 
             #  Is the player eligible to split their cards?
-            if playerhand[0][0] == playerhand[1][0] and roundcount == 1:
+            if self.playerhand[0][0] == self.playerhand[1][0] and roundcount == 1:
                 playeroptions = ['Hit', 'Hold', 'Double down', 'Split']
                 splitflag = True
 
@@ -43,27 +51,11 @@ class BlackJack:
 
             #  Hit
             if choice == 1:
-                self.hit(playerhand, deck.pullRandomCard())
+                self.hit()
 
             #  Hold
             elif choice == 2:
-                playerholds = True
-                while deck.sumCards(dealerhand) <= 17:
-                    dealerhand.append(deck.pullRandomCard())
-                    deck.printHand(dealerhand)
 
-                if self.dealerHasBusted(dealerhand, deck):
-                    inf.prettyPrint('DEALER HAS BUSTED, YOU WIN')
-                    return [True, inf.playAgain()]
-                elif deck.sumCards(dealerhand) > deck.sumCards(playerhand):
-                    inf.prettyPrint('DEALER HAS WON, YOU LOSE')
-                    return [False, inf.playAgain()]
-                elif deck.sumCards(dealerhand) < deck.sumCards(playerhand):
-                    inf.prettyPrint('YOU HAVE WON, YOU WIN BIG MONEY BRO')
-                    return [True, inf.playAgain()]
-                else:
-                    inf.prettyPrint('TIE, NO ONE WINS')
-                    return [True, inf.playAgain()]
 
             #  Double down
             elif choice == 3:               # Double down
@@ -76,27 +68,46 @@ class BlackJack:
 
 
             #  Check dealer cards
-            if deck.sumCards(dealerhand) <= 17:
-                dealerhand.append(deck.pullRandomCard())
-                if deck.sumCards(dealerhand) == 21:
+            if self.deck.sumCards(self.dealerhand) <= 17:
+                self.dealerhand.append(self.deck.pullRandomCard())
+                if self.deck.sumCards(self.dealerhand) == 21:
                     inf.prettyPrint('DEALER HAS 21 -- YOU LOSE')
                     return [False, inf.playAgain()]
 
-            roundcount += 1
+            self.roundcount += 1
 
-    def hit(self, hand, card):
-        hand.append(card)
+    def hit(self):
+        self.playerhand.append(self.deck.pullRandomCard())
+
+    def hold(self):
+    self.playerholds = True
+    while self.deck.sumCards(self.dealerhand) <= 17:
+        self.dealerhand.append(self.deck.pullRandomCard())
+        inf.printHand(self.dealerhand)
+
+    if self.dealerHasBusted(self.dealerhand, self.deck):
+        inf.prettyPrint('DEALER HAS BUSTED, YOU WIN')
+        return [True, inf.playAgain()]
+    elif self.deck.sumCards(self.dealerhand) > self.deck.sumCards(self.playerhand):
+        inf.prettyPrint('DEALER HAS WON, YOU LOSE')
+        return [False, inf.playAgain()]
+    elif self.deck.sumCards(self.dealerhand) < self.deck.sumCards(self.playerhand):
+        inf.prettyPrint('YOU HAVE WON, YOU WIN BIG MONEY BRO')
+        return [True, inf.playAgain()]
+    else:
+        inf.prettyPrint('TIE, NO ONE WINS')
+        return [True, inf.playAgain()]
 
     def doubleDown(self):
         pass
 
-    def split(self, hand):
+    def split(self):
         pass
 
-    def dealerPullCard(self, deck):
+    def dealerPullCard(self):
         pass
 
-    def dealerHasBusted(self, hand, deck):
-        if deck.sumCards(hand) > 21:
+    def dealerHasBusted(self):
+        if self.deck.sumCards(hand) > 21:
             return True
         return False
